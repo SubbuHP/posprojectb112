b45e8927f228a48f32bce487592ee8a48a9dea47
subramaniam <subbu.bangaloree8@gmail.com>
2024-10-30 13:11:24 +0530

    Added ErrMsg to LoginPage POM class

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 
 public class LoginPage {
 
@@ -15,6 +18,9 @@ public class LoginPage {
 	
 	@FindBy(name="login-button")
 	private WebElement goBTN;
+	
+	@FindBy(xpath="//div[@class='error']")
+	private WebElement errMsg;
 	
 	public LoginPage(WebDriver driver)
 	{
@@ -36,4 +42,19 @@ public class LoginPage {
 		goBTN.click();
 	}
 	
+	public boolean verifyErrMsgIsDisplayed(WebDriverWait wait)
+	{
+		try
+		{
+			wait.until(ExpectedConditions.visibilityOf(errMsg));
+			Reporter.log("Err Page is Displayed",true);
+			return true;
+		}
+		catch(Exception e)
+		{
+			Reporter.log("Err Msg is Not Displayed",true);
+			return false;
+		}
+	
+    }
 }
